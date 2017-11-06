307b676f207dd841e3c7a65b1c778379da31f089
Paul Nguyen <paul.nguyen@sjsu.edu>
2017-11-05 21:48:02 -0800

    update

---
+++ src/main/main_test.go
@@ -0,0 +1,20 @@
+
+/*
+	Gumball API in Go (Version 1)
+	Basic Version with Wercker
+*/
+
+package main
+
+import (
+	"testing"
+)
+
+func TestGumball(t *testing.T) {  
+    cnt := machine.CountGumballs
+    if cnt != 989 {
+       t.Errorf("Count of Gumballs Incorrect, got: %d, want: %d.", cnt, 989)
+    }
+}
+
+
